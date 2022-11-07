ed5d48558b84e0c58474edabce3a28c460b9675f
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2022-11-07 13:44:28 -0800

    feat: Add Atom feed

--- src/lib/util.js
+++ src/lib/util.js
@@ -25,7 +25,36 @@ const DATA_DIR = path.resolve(__dirname, "../data");
 // Exports
 //-----------------------------------------------------------------------------
 
-export async function loadBlogPosts(posts) {
+export async function loadBlogPostsX(posts) {
+
+    return posts.map(post => {
+
+        const filename = path.basename(post.file, ".md");
+        const urlParts = {
+            year: filename.slice(0, 4),
+            month: filename.slice(5, 7),
+            slug: filename.slice(11)
+        };
+
+        const newPost = Object.create(post, {
+            url: { value: `/blog/${urlParts.year}/${urlParts.month}/${urlParts.slug}/` }
+        });
+
+        newPost.urlParts = urlParts;
+        newPost.frontmatter.date = new Date(filename.slice(0, 10));
+        newPost.frontmatter.pubDate = newPost.frontmatter.date;
+        return newPost;
+    });
+
+}
+
+export async function loadBlogPosts() {
+
+    const postFiles = await import.meta.glob("../pages/_posts/**/*.md");
+    const posts = await Promise.all(Object.values(postFiles).map(async (getInfo) => {
+        const meta = await getInfo();
+        return meta;
+    }));
 
     return posts.map(post => {
 
